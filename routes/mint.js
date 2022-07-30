a23cfaf4331bb1a2a60a3986ba4ff1bbf2572cf5
youngho-j <alfkwl1239@naver.com>
2022-07-30 23:43:51 +0900

    bsm 프로젝트
    1. 레이아웃에 따른 url 경로 추가

--- routes/mint.js
+++ routes/mint.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const conn = require("../database/mysql.js");
 const moment = require("moment");
 
+// nft 생성 후 판매 가격 설정
 router.get("/mint", (req, res) => {
     res.render("mintForm");
 });
@@ -9,5 +10,26 @@ router.get("/mint", (req, res) => {
 router.get("/setPrice", (req, res) => {
     res.render("priceForm");
 });
+
+// nft 구매
+router.get("/buy", (req, res) => {
+    res.sender("nft 곡 구매 페이지 - modal");
+});
+
+// nft 판매
+router.get("/sell", (req, res) => {
+    res.sender("nft 곡 판매 페이지 - modal");
+});
+
+// nft 전송
+router.get("/transfer", (req, res) => {
+    res.sender("nft 곡 전송 페이지 - modal / 에어드랍");
+});
+
+// nft 목록 출력 - 전체 / 판매 목록 / 개인 목록 
+router.get("/list", (req, res) => {
+    res.sender("nft 전체 목록(각 데이터 포함) / 주소를 통해 목록을 가져올 수 도 있어여함");
+});
+
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -5,4 +5,16 @@ router.get("/myPage", (req, res) => {
     res.render("myPage");
 });
 
+router.get("/login", (req, res) => {
+    res.sender("로그인페이지");
+});
+
+router.post("/login", (req, res) => {
+    res.sender("로그인 - 이후 필요정보 세션에 추가");
+});
+
+router.get("/logout", (req, res) => {
+    res.sender("로그 아웃 - 지갑 연동 해제");
+});
+
 module.exports = router;
