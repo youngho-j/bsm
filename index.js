31c3478cc8de3abaf33606c1a40537a4855af15c
youngho-j <alfkwl1239@naver.com>
2022-07-31 23:19:05 +0900

    BSM 프로젝트
    1. loopple 템플릿 적용
     - topNav, footer 분리

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ require("dotenv");
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended:false
