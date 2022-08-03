97ed4254bd24f61cec6e5b437b459b566be5b36c
youngho-j <alfkwl1239@naver.com>
2022-08-03 17:16:46 +0900

    BSM 프로젝트
    1.  로고 이미지 추가, 적용 및 불필요한 코드 제거
      - bsm_logo 이미지 추가
    
    2. 회원가입 기능 추가 [팀원]
      - caver-js-ext-kas 추가
      - 회원 가입시 KAS 사용하여 address 생성
      - 계정 생성시 변경을 못하도록 readOnly 적용

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,65 @@
 const router = require("express").Router();
 const conn = require("../database/mysql.js");
+// env 파일을 읽기 위한 선언
+require("dotenv").config();
+
+// Klaytn API Service 설정
+const CaverExtKAS = require("caver-js-ext-kas");
+const caver = new CaverExtKAS();
+// Klaytn API Service 초기화
+caver.initKASAPI(process.env.chainId, process.env.accesskey, process.env.secretkey);
+
+// 지갑 klaytn address 생성
+router.get("/getWalletAddress", (req, res) => {
+    // kas를 통해 지갑 주소 json 형식으로 생성
+    account = async() => {
+        res.json(await caver.kas.wallet.createAccount());
+    }
+    account();
+});
+
+// 회원 가입 페이지 이동 
+router.get("/signup", (req, res) => {
+    res.render("signup")
+});
+
+// 회원 가입 진행 
+router.post("/signup", (req, res) => {
+    let email = req.body._email;
+    let pass = req.body._pass;
+    let address = req.body._address;
+
+    conn.query(
+        `select * from user where email = ?`,
+        [email],
+        (err, result) => {
+            //select문 조회시 에러
+            if(err) {
+                console.log(err);
+                res.send("SQL select Error");
+            } else {
+                // 조회시 값이 없는 경우 => 사용 가능한 이메일
+                if(result.length == 0) {
+                    conn.query(
+                        `insert into user values (?, ?, ?)`,
+                        [email, pass, address],
+                        (err) => {
+                            if(err) {
+                                console.log(err);
+                                res.send("SQL insert Error");
+                            } else {
+                                res.redirect('/user/signin');
+                            }
+                        } 
+                    )
+                } else {
+                    // 해당 값이 존재할 경우 메세지
+                    res.send("이메일이 존재합니다.")
+                }
+            }
+        }
+    )
+});
 
 // indexjs에서 user 경로로 들어올 경우 해당 js 파일로 전달됨
 
@@ -14,14 +74,6 @@ router.get("/signin", (req, res) => {
 
 router.post("/signin", (req, res) => {
     res.sender("로그인 - 이후 필요정보 세션에 추가");
-});
-
-router.get("/signup", (req, res) => {
-    res.render("signup")
-});
-
-router.post("/signup", (req, res) => {
-    res.sender("회원가입");
 });
 
 router.get("/logout", (req, res) => {
