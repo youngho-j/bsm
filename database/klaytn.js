bd05b7a8defdb27a1c46663858eddf7c2a2d1bba
youngho-j <alfkwl1239@naver.com>
2022-08-06 15:41:25 +0900

    BSM 프로젝트
    1.  nft 컨트랙트 추가
      - 컨트랙 민팅/조회 기능 - 팀원 J
      - 현재 컨트랙 배포시 NFT 명, 심볼 설정하는 부분 수정 필요
    
    2.  민팅 기능 수정
     - mintForm view, select value, 발급  수정
     - 추후 시간 남을 경우 range 초기값 보이도록 설정해보기
    
    3.  klaytn 추가
     - 기존 KAS API 및 KeytingContainer module화
    
    4. nftjs 수정
     - caverjs 모듈 추가
     - klaytn 테스트 넷 접속 기능 추가
     - 컨트랙 연동
     - mintForm에서 넘어온 정보를 json화 하여 s3 버킷에 업로드
     - 컨트랙에 tokenUri를 넘겨 NFT 민팅

---
+++ database/klaytn.js
@@ -0,0 +1,23 @@
+// env 파일을 읽기 위한 선언
+require("dotenv").config();
+
+// Klaytn API Service 설정 - 따로 작성하기 
+const CaverExtKAS = require("caver-js-ext-kas");
+
+// Klaytn API Service 인스턴스 생성 후 변수 할당
+const kasApi = new CaverExtKAS();
+
+// in-memory wallet인 KeyringContainer 사용하여 외부지갑 등록 인스턴스 생성
+const keyringContainer = new kasApi.keyringContainer();
+
+// keyringContainer에 등록하여 사용할 외부 지갑 privatekey를 등록해줌
+const keyring = keyringContainer.keyring.createFromPrivateKey(process.env.mainPK) 
+keyringContainer.add(keyring); // 새로운 월렛 추가(KAS 지갑주소가 아닌 외부 지갑 주소)
+
+// Klaytn API Service 초기화 - 사용하기 위해 정보를 넣어줌 / 현재 chainId 1001(test-net)
+kasApi.initKASAPI(process.env.chainId, process.env.accesskey, process.env.secretkey);
+
+module.exports = {
+    kasApi,
+    keyring
+};
--- routes/nft.js
+++ routes/nft.js
@@ -3,7 +3,20 @@ const conn = require('../database/mysql.js');
 const moment = require('moment');
 const fetch = require('cross-fetch');
 const fs = require('fs');
+const caverjs = require("caver-js");
+const klaytn = require('../database/klaytn.js');
+const contractEnv = require("../contract.json");
 require("dotenv").config();
+
+// caverjs를 이용하여 klaytn test net 접속을 위한 경로 설정
+const testConn = new caverjs("https://api.baobab.klaytn.net:8651");
+
+// 컨트랙 실행을 위한 abi 및 CA 등록 
+const smartContract = new testConn.klay.Contract(contractEnv.abi, contractEnv.ca);
+
+// 지갑 주소, 프라이빗 키 등록
+const account = testConn.klay.accounts.createWithAccountKey(process.env.mainWA, process.env.mainPK);
+testConn.klay.accounts.wallet.add(account);
 
 // indexjs에서 nft 경로로 들어올 경우 해당 js 파일로 전달됨
 
@@ -11,6 +24,7 @@ require("dotenv").config();
 router.get('/mint', (req, res) => {
     res.render('mintForm');
 });
+
 // nft 판매 가격 설정 페이지 이동
 router.get('/setPrice', (req, res) => {
     res.render('priceForm');
@@ -39,7 +53,7 @@ router.get('/list', (req, res) => {
 
 // nft 민팅 정보 설정값 DB 저장
 router.post('/mint', (req, res) => {
-    
+    // 추후 민팅하려고 하는 사용자의 Address 받아야함 
     let title = req.body._title;
     let artist = req.body._artist;
     let genre = req.body._genre;
@@ -74,24 +88,38 @@ router.post('/mint', (req, res) => {
             "Content-Type" : "application/json",
         },
         body : metadataJSON
-    }).catch((err) => {
-        console.log("원격 호출 에러 : " + err);
-    }).then(() => {
+    })
+    .catch((err) => console.log("원격 호출 에러 : " + err))
+    .then(() => {
         console.log("업로드 완료!");
-        conn.query(
-            `insert into nft(title, artist, genre, amount, fee, imgUrl, musicUrl) values(?,?,?,?,?,?,?)`,
-            [title, artist, genre, amount, fee, imgUrl, musicUrl],
-            (err) => {
-                if(err) {
-                    console.log(err);
-                } else {
-                    res.redirect("/");
-                }
+        // conn.query(
+        //     `insert into nft(title, artist, genre, amount, fee, imgUrl, musicUrl) values(?,?,?,?,?,?,?)`,
+        //     [title, artist, genre, amount, fee, imgUrl, musicUrl],
+        //     (err) => {
+        //         if(err) {
+        //             console.log(err);
+        //         } else {
+        //             res.redirect("/");
+        //         }
+        //     }
+        // )
+        // 현재 NFT 이름이 배포시 적용했던 이름과 심볼로 들어감
+        // 추후 민팅하는 사람의 Address로 변경해야함
+        smartContract.methods.mintWithTokenURI(
+            "0x870b53fac31b735cb1f95D49e3E790144F2A6b78", amount, dataUrl)
+        .send(
+            {
+                from : account.address,
+                gas : 100000000
             }
         )
+        .then(result => {
+            console.log("민팅 완료!");
+        })
+        .catch(error => console.log("민팅 에러", error))
     })
-    // JSON 저장 
-    // fs.writeFileSync(dataUrl, metadataJSON);
+    // alert창을 띄워주기 고민해볼 것
+    res.redirect("/");
 });
 
 // nft 판매 정보 등록 - 파일 값 제외하고 DB 저장 여부 확인
